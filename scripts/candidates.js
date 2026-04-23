dc0e10a7d281a201c8de1cbb4d269adf8ab61e43
federicoacker <acker.federico@gmail.com>
2026-04-23 10:08:13 +0200

    Aggiunti i Candidati

---
+++ scripts/candidates.js
@@ -0,0 +1,13 @@
+const candidates = [
+  { id: 1, label: "Militante nero", reveal: "È un pacifista, esperto in non-violenza e gestione creativa dei conflitti." },
+  { id: 2, label: "Poliziotto con fucile", reveal: "È stato radiato dalla Polizia perché per vendetta ha ammazzato un'intera famiglia." },
+  { id: 3, label: "Atleta", reveal: "È una donna di 70 anni che ha vinto le Olimpiadi delle 'Grey Panthers'." },
+  { id: 4, label: "Architetto", reveal: "Mangia solo rape rosse, pianta che non sembra allignare nel nuovo pianeta." },
+  { id: 5, label: "Cuoca", reveal: "Ha lavorato in un carcere e ha imparato a cucinare solo quel tipo di pietanze." },
+  { id: 6, label: "Falegname cieco", reveal: "È un famoso maestro delle costruzioni in legno e potrebbe insegnare queste abilità a chiunque." },
+  { id: 7, label: "Dottoressa", reveal: "È una laureata in legge, dirigente nella Pubblica Amministrazione." },
+  { id: 8, label: "Prostituta", reveal: "È un'ottima cuoca, giovane, allegra e gode di ottima salute." },
+  { id: 9, label: "Ragazza di 16 anni incinta", reveal: "Ha l'AIDS." },
+  { id: 10, label: "Musicista gay", reveal: "Sa suonare solo l'organo." },
+  { id: 11, label: "Sacerdote", reveal: "È il capo di una setta fondamentalista musulmana." },
+]
